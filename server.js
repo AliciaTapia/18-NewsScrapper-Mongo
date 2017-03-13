7657e4e3aab0497da8282777566bcb1b8e2a5a37
Alicia <Alis@perlas-mbp.attlocal.net>
2017-03-12 19:19:15 -0500

    heroku login ang mongoose.conect

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ var Article = require("./models/article.js");
 // Our scraping tools
 var request = require("request");
 var cheerio = require("cheerio");
+
+var PORT = process.env.PORT || 3000;
 // Set mongoose to leverage built in JavaScript ES6 Promises
 mongoose.Promise = Promise;
 
@@ -28,6 +30,8 @@ app.use(express.static("public"));
 
 // Database configuration with mongoose
 mongoose.connect("mongodb://localhost/news");
+mongoose.connect("mongodb://heroku_gfl44np2:n9dg33qgkqtftultnegogsv5gr@ds125060.mlab.com:25060/heroku_gfl44np2");
+
 var db = mongoose.connection;
 
 // Show any mongoose errors
@@ -50,8 +54,8 @@ require("./routes/saved-routes.js")(app);
 
 
 // Listen on port 3000
-app.listen(3000, function() {
-  console.log("App running on port 3000!");
-});
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
 
 
